cb0cade3fea33b597695543db421bba3b7ef4368
Deep Patel <patel.deep041@gmail.com>
2026-07-06 22:49:02 +0530

    fix: update API URL in environment configuration for production deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'http://139.59.47.24:3000/',
     gtmId: 'G-DX4SLSSE86',
     encryptionEnabled: true,
     encryptionKey: 'invoicing-api-encryption-key'
